0c4d1d94e60190b6548374916644be28cce3e084
Brad <brad4au57@gmail.com>
2018-07-07 13:17:25 -0500

    Added sample config.js file

---
+++ config-example.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  consumer_key:         '...', //Copy & paste the consumer key provided from twitter
+  consumer_secret:      'BFClhdoZQTTXMPOMYfGY5XQC30rENgrzUZQsVVheXOyXPFRwgd', //Copy & paste the consumer secret key provided from twitter
+  access_token:         '899473339324002304-IumN1EkmKLvDyvJgol6PjXzBgHyNNqc', //Generate a new access token then copy & paste here from twitter
+  access_token_secret:  'niRqHqtUq7SAEKcsPO0u5Vnuzj0kn1eUMWGp9TRPgWJg7', //Generates with new access token then copy & paste from twitter
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
