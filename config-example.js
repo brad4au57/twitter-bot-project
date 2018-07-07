fb7ee0be1bf0941cee28d5e05dea656f0e719377
brad4au57 <brad4au57@gmail.com>
2018-07-07 13:28:08 -0500

    Removed keys from config example

--- config-example.js
+++ config-example.js
@@ -1,9 +1,10 @@
 
 module.exports = {
   consumer_key:         '...', //Copy & paste the consumer key provided from twitter
-  consumer_secret:      'BFClhdoZQTTXMPOMYfGY5XQC30rENgrzUZQsVVheXOyXPFRwgd', //Copy & paste the consumer secret key provided from twitter
-  access_token:         '899473339324002304-IumN1EkmKLvDyvJgol6PjXzBgHyNNqc', //Generate a new access token then copy & paste here from twitter
-  access_token_secret:  'niRqHqtUq7SAEKcsPO0u5Vnuzj0kn1eUMWGp9TRPgWJg7', //Generates with new access token then copy & paste from twitter
+  consumer_secret:      '...', //Copy & paste the consumer secret key provided from twitter
+  access_token:         '...', //Generate a new access token then copy & paste here from twitter
+  access_token_secret:  '...', //Generates with new access token then copy & paste from twitter
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+
 }
