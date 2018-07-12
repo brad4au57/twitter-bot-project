ac04beb149d271cb5b26c6393c3f357404e5cf09
brad4au57 <brad4au57@gmail.com>
2018-07-11 21:48:25 -0500

    Initializing twitter bot

---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  consumer_key:         'pmj7pyXsMBbDgSLdc7Gn5b5Dh',
+  consumer_secret:      'BFClhdoZQTTXMPOMYfGY5XQC30rENgrzUZQsVVheXOyXPFRwgd',
+  access_token:         '899473339324002304-IumN1EkmKLvDyvJgol6PjXzBgHyNNqc',
+  access_token_secret:  'niRqHqtUq7SAEKcsPO0u5Vnuzj0kn1eUMWGp9TRPgWJg7',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  userName: "brad4au57"
+}
